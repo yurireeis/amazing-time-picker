bf212898461ea9eb0c04daed3117f97fc511237b
Yuri Reis <yreis@acer-yreis.koerich.com.br>
2019-10-03 11:51:49 -0300

    Workaround: build version of amazing-time-picker BR terms

---
+++ src/app/atp-library/preferences.d.ts
@@ -0,0 +1,6 @@
+import { IDisplayPreference } from './definitions';
+export declare const PersianPreference: IDisplayPreference;
+export declare const ArabicPreference: IDisplayPreference;
+export declare const ChinesePreference: IDisplayPreference;
+export declare const BrazilianPreference: IDisplayPreference;
+export declare const Preference: (locale: string) => IDisplayPreference;
